a26e999e4e3a05a17e86edee32721485b2913d88
Matteus <matteus@tolfix.com>
2021-07-02 11:20:33 +0200

    Update index.d.ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -65,13 +65,16 @@ declare module 'discord.js' {
 }
 
 
-export enum MessageComponentTypes {
+declare module 'discord-buttons' {
+  export default function (client: discord.Client): void;
+
+  export enum MessageComponentTypes {
     ACTION_ROW = 1,
     BUTTON = 2,
-    SELECT_MENU = 3
-}
+    SELECT_MENU = 3,
+  }
 
-export enum MessageButtonStyles {
+  export enum MessageButtonStyles {
     blurple = 1,
     grey = 2,
     green = 3,
@@ -84,46 +87,61 @@ export enum MessageButtonStyles {
     SECONDARY = 2,
     SUCCESS = 3,
     DESTRUCTIVE = 4,
-    LINK = 5
-}
+    LINK = 5,
+  }
 
-export type MessageButtonStyle = keyof typeof MessageButtonStyles;
+  export type MessageButtonStyle = keyof typeof MessageButtonStyles;
 
-export type MessageButtonStyleResolvable = MessageButtonStyle | MessageButtonStyles;
+  export type MessageButtonStyleResolvable = MessageButtonStyle | MessageButtonStyles;
 
-export interface GuildButtonEmoji {
-    name?: string,
-    id?: Snowflake,
-    animated?: boolean
-}
+  export interface GuildButtonEmoji {
+    name?: string;
+    id?: Snowflake;
+    animated?: boolean;
+  }
 
-export interface MessageButtonOptions {
-    type: MessageComponentTypes.BUTTON,
-    style: MessageButtonStyles,
-    label?: string,
-    disabled?: boolean,
-    emoji?: string | GuildButtonEmoji,
-    url?: string,
-    id?: string,
-    custom_id?: string
-}
+  export interface MessageButtonOptions {
+    type: MessageComponentTypes.BUTTON;
+    style: MessageButtonStyleResolvable;
+    label?: string;
+    disabled?: boolean;
+    emoji?: string | GuildButtonEmoji;
+    url?: string;
+    id?: string;
+    custom_id?: string;
+  }
 
-export interface MessageButtonData {
-    type?: MessageComponentTypes.BUTTON,
-    style: MessageButtonStyles | number,
-    label?: string,
-    disabled?: boolean,
-    emoji?: GuildButtonEmoji,
-    url?: string,
-    custom_id?: string
-}
+  export interface MessageButtonData {
+    type?: MessageComponentTypes.BUTTON;
+    style: MessageButtonStyles | number;
+    label?: string;
+    disabled?: boolean;
+    emoji?: GuildButtonEmoji;
+    url?: string;
+    custom_id?: string;
+  }
 
-export interface MessageActionRowData {
-    type: number | string,
-    components: MessageButton[]
-}
+  export interface MessageActionRowData {
+    type: number | string;
+    components: MessageButton[];
+  }
 
-export class MessageComponent {
+  export class InteractionReply {
+    constructor(client: discord.Client, component: MessageComponent, webhook: WebhookClient);
+    client: discord.Client;
+    component: MessageComponent;
+    webhook: WebhookClient;
+    has: boolean;
+    isEphemeral: boolean;
+    send(content: APIMessageContentResolvable | (MessageOptions & { split?: false }) | MessageAdditions): Promise<any>;
+    edit(content: APIMessageContentResolvable | (MessageOptions & { split?: false }) | MessageAdditions): Promise<any>;
+    defer(ephemeral: boolean): Promise<any>;
+    think(ephemeral: boolean): Promise<any>;
+    fetch(): Promise<any>;
+    delete(): Promise<void>;
+  }
+
+  export class MessageComponent {
     constructor(client: discord.Client, data: any);
     client: discord.Client;
     id: Snowflake;
@@ -134,33 +152,22 @@ export class MessageComponent {
     guild: Guild;
     channel: Channel;
     clicker: {
-        user: User;
-        member: GuildMember;
-        fetch: () => Promise<boolean>;
+      id: Snowflake;
+      user: User;
+      member: GuildMember;
+      fetch: () => Promise<boolean>;
     };
     message: Message;
     webhook: WebhookClient;
-    replied: boolean;
-    deferred: boolean;
-    defer(ephemeral?: boolean): Promise<void>;
-    think(ephemeral?: boolean): Promise<void>;
-    followUp(content: string, options?: {}): Promise<void>;
-    get reply(): {
-        send: (
-            content: APIMessageContentResolvable | (ReplyOptions & { split?: false, ephemeral?: boolean }) | MessageAdditions,
-        ) => Promise<any>;
-        fetch: () => Promise<any>;
-        edit: (content: any, options?: {}) => Promise<any>;
-        delete: () => Promise<void>;
-    };
-}
+    reply: InteractionReply;
+  }
 
-export class BaseMessageComponent {
-    static create(data: MessageActionRow | MessageButton): MessageActionRow | MessageButton;
+  export class BaseMessageComponent {
     constructor(data: MessageActionRow | MessageButton);
-}
+    private static create(data: MessageActionRow | MessageButton): MessageActionRow | MessageButton;
+  }
 
-export class MessageActionRow extends BaseMessageComponent {
+  export class MessageActionRow extends BaseMessageComponent {
     constructor(data?: {});
     setup(data: any): MessageActionRow;
     component: MessageButton;
@@ -168,12 +175,12 @@ export class MessageActionRow extends BaseMessageComponent {
     addComponents(...components: MessageButton[]): MessageActionRow;
     addComponent(component: MessageButton): MessageActionRow;
     toJSON(): {
-        components: MessageButton[];
-        type: string | number;
+      components: MessageButton[];
+      type: string | number;
     };
-}
+  }
 
-export class MessageButton extends BaseMessageComponent {
+  export class MessageButton extends BaseMessageComponent {
     constructor(data?: MessageButton | MessageButtonData | MessageButtonOptions);
     public setup(data: any): MessageButton;
     public style: MessageButtonStyles;
@@ -184,65 +191,49 @@ export class MessageButton extends BaseMessageComponent {
     public custom_id: string;
     public setStyle(style: MessageButtonStyleResolvable): MessageButton;
     public setLabel(label: string): MessageButton;
-    /**
-     * 
-     * @param disabled 
-     * @description
-     * Disables the button
-     */
     public setDisabled(disabled?: boolean): MessageButton;
     public setURL(url: string): MessageButton;
     public setID(id: string): MessageButton;
     public setEmoji(emoji: any): MessageButton;
     public toJSON(): MessageButtonData;
-}
+  }
 
-export interface MessageOptions extends discord.MessageOptions {
+  export interface MessageOptions extends discord.MessageOptions {
     component?: MessageButton | MessageActionRow;
     components?: MessageActionRow[];
     button?: MessageButton | MessageButton[];
     buttons?: MessageButton | MessageButton[];
-}
+  }
 
-export interface ReplyOptions extends MessageOptions {
-    ephemeral?: boolean,
-    flags?: number
-}
+  export interface ReplyOptions extends MessageOptions {
+    ephemeral?: boolean;
+    flags?: number;
+  }
 
-export class WebhookClient extends discord.WebhookClient {
+  export class WebhookClient extends discord.WebhookClient {
     editMessage(message: string, content: any, options?: {}): Promise<any>;
     deleteMessage(message: string): Promise<void>;
     fetchMessage(message: string, cache?: boolean): Promise<any>;
-}
+  }
 
-export interface Message extends discord.Message {
+  export interface Message extends discord.Message {
     components: MessageActionRow[];
-    createButtonCollector(
-        filter: CollectorFilter,
-        options?: AwaitMessageButtonOptions
-    ): ButtonCollector;
-    awaitButtons(
-        filter: CollectorFilter,
-        options?: AwaitMessageButtonOptions
-    ): Promise<Collection<Snowflake, MessageComponent>>;
-}
+    createButtonCollector(filter: CollectorFilter, options?: AwaitMessageButtonOptions): ButtonCollector;
+    awaitButtons(filter: CollectorFilter, options?: AwaitMessageButtonOptions): Promise<Collection<Snowflake, MessageComponent>>;
+  }
 
-export interface MessageButtonCollectorOptions extends CollectorOptions {
+  export interface MessageButtonCollectorOptions extends CollectorOptions {
     max?: number;
     maxButtons?: number;
     maxUsers?: number;
-}
+  }
 
-export interface AwaitMessageButtonOptions extends MessageButtonCollectorOptions {
+  export interface AwaitMessageButtonOptions extends MessageButtonCollectorOptions {
     errors?: string[];
-}
+  }
 
-export class ButtonCollector extends Collector<Snowflake, MessageComponent> {
-    constructor(
-        message: Message,
-        filter: CollectorFilter,
-        options?: MessageButtonCollectorOptions
-    );
+  export class ButtonCollector extends Collector<Snowflake, MessageComponent> {
+    constructor(message: Message, filter: CollectorFilter, options?: MessageButtonCollectorOptions);
     message: Message;
     users: Collection<Snowflake, User>;
     total: number;
@@ -254,61 +245,51 @@ export class ButtonCollector extends Collector<Snowflake, MessageComponent> {
 
     collect(button: MessageButton): Snowflake;
     dispose(button: MessageButton): Snowflake;
-    on(
-        event: 'collect' | 'dispose',
-        listener: (interaction: MessageComponent) => Awaited<void>,
-    ): this;
-    on(
-        event: 'end',
-        listener: (collected: Collection<Snowflake, MessageComponent>, reason: string) => Awaited<void>,
-    ): this;
+    on(event: 'collect' | 'dispose', listener: (interaction: MessageComponent) => Awaited<void>): this;
+    on(event: 'end', listener: (collected: Collection<Snowflake, MessageComponent>, reason: string) => Awaited<void>): this;
     on(event: string, listener: (...data: any[]) => Awaited<void>): this;
 
-    once(
-        event: 'collect' | 'dispose',
-        listener: (interaction: MessageComponent) => Awaited<void>,
-    ): this;
-    once(
-        event: 'end',
-        listener: (collected: Collection<Snowflake, MessageComponent>, reason: string) => Awaited<void>,
-    ): this;
+    once(event: 'collect' | 'dispose', listener: (interaction: MessageComponent) => Awaited<void>): this;
+    once(event: 'end', listener: (collected: Collection<Snowflake, MessageComponent>, reason: string) => Awaited<void>): this;
     once(event: string, listener: (...data: any[]) => Awaited<void>): this;
-}
+  }
 
-export type MessageAdditions = MessageEmbed | MessageAttachment | MessageButton | MessageActionRow | (MessageEmbed | MessageAttachment | MessageButton | MessageActionRow)[];
+  export type MessageAdditions =
+    | MessageEmbed
+    | MessageAttachment
+    | MessageButton
+    | MessageActionRow
+    | (MessageEmbed | MessageAttachment | MessageButton | MessageActionRow)[];
 
-export type Awaited<T> = T | Promise<T>;
+  export type Awaited<T> = T | Promise<T>;
 
-export interface ExtendedTextChannel extends discord.TextChannel {
-    send(content: APIMessageContentResolvable | (MessageOptions & { split?: false }) | MessageAdditions,): Promise<Message>;
+  export interface ExtendedTextChannel extends discord.TextChannel {
+    send(content: APIMessageContentResolvable | (MessageOptions & { split?: false }) | MessageAdditions): Promise<Message>;
     send(options: MessageOptions & { split: true | SplitOptions }): Promise<Message[]>;
     send(options: MessageOptions | discord.APIMessage): Promise<Message | Message[]>;
     send(content: StringResolvable, options: (MessageOptions & { split?: false }) | MessageAdditions): Promise<Message>;
     send(content: StringResolvable, options: MessageOptions & { split: true | SplitOptions }): Promise<Message[]>;
     send(content: StringResolvable, options: MessageOptions): Promise<Message | Message[]>;
     send(content: StringResolvable, options: MessageButton | MessageActionRow): Promise<Message | Message[]>;
-}
+  }
 
-export interface ExtendedDMChannel extends discord.DMChannel {
-    send(content: APIMessageContentResolvable | (MessageOptions & { split?: false }) | MessageAdditions,): Promise<Message>;
+  export interface ExtendedDMChannel extends discord.DMChannel {
+    send(content: APIMessageContentResolvable | (MessageOptions & { split?: false }) | MessageAdditions): Promise<Message>;
     send(options: MessageOptions & { split: true | SplitOptions }): Promise<Message[]>;
     send(options: MessageOptions | discord.APIMessage): Promise<Message | Message[]>;
     send(content: StringResolvable, options: (MessageOptions & { split?: false }) | MessageAdditions): Promise<Message>;
     send(content: StringResolvable, options: MessageOptions & { split: true | SplitOptions }): Promise<Message[]>;
     send(content: StringResolvable, options: MessageOptions): Promise<Message | Message[]>;
     send(content: StringResolvable, options: MessageButton | MessageActionRow): Promise<Message | Message[]>;
-}
+  }
 
-export interface ExtendedNewsChannel extends discord.NewsChannel {
-    send(content: APIMessageContentResolvable | (MessageOptions & { split?: false }) | MessageAdditions,): Promise<Message>;
+  export interface ExtendedNewsChannel extends discord.NewsChannel {
+    send(content: APIMessageContentResolvable | (MessageOptions & { split?: false }) | MessageAdditions): Promise<Message>;
     send(options: MessageOptions & { split: true | SplitOptions }): Promise<Message[]>;
     send(options: MessageOptions | discord.APIMessage): Promise<Message | Message[]>;
     send(content: StringResolvable, options: (MessageOptions & { split?: false }) | MessageAdditions): Promise<Message>;
     send(content: StringResolvable, options: MessageOptions & { split: true | SplitOptions }): Promise<Message[]>;
     send(content: StringResolvable, options: MessageOptions): Promise<Message | Message[]>;
     send(content: StringResolvable, options: MessageButton | MessageActionRow): Promise<Message | Message[]>;
-}
-
-declare module 'discord-buttons' {
-    export default function (client: discord.Client): void;
+  }
 }
